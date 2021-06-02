0339be9e78bd17cce2dfa5c52db7df1583586cd7
zkingzheng <291756868@qq.com>
2021-06-02 18:05:19 +0800

    1.1

---
+++ miniprogram/app.js
@@ -0,0 +1,17 @@
+// app.js
+App({
+  onLaunch() {
+     // 初始化云开发环境
+     if (!wx.cloud) {
+      console.error('请使用 2.2.3 或以上的基础库以使用云能力')
+  } else {
+      wx.cloud.init({
+          env: 'test-tn54w',
+          traceUser: true,
+      })
+  }
+  },
+  globalData: {
+    userInfo: null
+  }
+})
