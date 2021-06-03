b3024ce75218ed0fd41055875f2dd8c6d3d33b01
zkingzheng <291756868@qq.com>
2021-06-04 00:43:43 +0800

    6.4

--- miniprogram/CalculatorPackage/calculator/calculator.js
+++ miniprogram/CalculatorPackage/calculator/calculator.js
@@ -7,13 +7,24 @@ Page({
   data: {
     calcul:{
       navActive:0,
-      year:20,
-      repayment:0,
       calculator:0,
-      price:'',
-      lpr:4.9,
-      bp:0,
-      hl:0
+      repayment:0,
+      business:{
+        year:20,
+        price:'',
+        lpr:4.9,
+        bp:0,
+        hl:0
+      },
+      provident:{
+        year:20,
+        price:'',
+        lpr:3.25,
+        bp:0,
+        hl:0
+      }
+      
+      
     }
    
   },
@@ -61,16 +72,28 @@ Page({
       [`calcul.navActive`]:e.currentTarget.dataset.id
     })
   },
-  _hankYear(e){
+  _hankBusinessYear(e){
     this.setData({
-      [`calcul.year`]:e.detail.value
+      [`calcul.business.year`]:e.detail.value
     })
   },
-  _hankBp(e){
-    let lpr=this.data.calcul.lpr;
+  _hankBusinessBp(e){
+    let lpr=this.data.calcul.business.lpr;
     this.setData({
-      [`calcul.bp`]:Number(e.detail.value),
-      [`calcul.hl`]:(Number(e.detail.value)*0.01+lpr).toFixed(2)
+      [`calcul.business.bp`]:Number(e.detail.value),
+      [`calcul.business.hl`]:(Number(e.detail.value)*0.01+lpr).toFixed(2)
+    })
+  },
+  _hankProvidentYear(e){
+    this.setData({
+      [`calcul.provident.year`]:e.detail.value
+    })
+  },
+  _hankProvidentBp(e){
+    let lpr=this.data.calcul.provident.lpr;
+    this.setData({
+      [`calcul.provident.bp`]:Number(e.detail.value),
+      [`calcul.provident.hl`]:(Number(e.detail.value)*0.01+lpr).toFixed(2)
     })
   }
 })
